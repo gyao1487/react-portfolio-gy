8100b1a3bdec02a9b5a2e4e9466d3e58cefe69db
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-05 16:44:21 -0600

    imported files from original repo

---
+++ src/components/ContentContainer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume/Resume";
+import Navbar from "./Navbar";
+
+function ContentContainer() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Contact":
+        return <Contact />;
+      case "Resume":
+        return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
+
+export default ContentContainer;
---
+++ src/components/pages/Portfolio/ProjectCards.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProjectCards(props) {
+
+}
+
+export default ProjectCards;
